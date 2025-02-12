21150ff04a8cfc0a7b4da6f24ed29790c5e62717
yuhi yamane <bird9.yuhi@gmail.com>
2025-02-12 22:04:33 +0900

    :hammer: refactor: モード別設定をviteに追加
    `vite.config.ts`にモードに応じたベースパス設定を追加。`package.json`にプロダクション用のビルドスクリプトを追加し、開発と本番のビルドを分離。

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from 'node:path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@/': path.resolve(__dirname, './src'),
-    }
-  }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        '@/': path.resolve(__dirname, './src'),
+      },
+    },
+    base: mode === 'production' ? '/ludiscan/view/' : '/',
+  };
 });
