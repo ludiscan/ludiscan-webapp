4c5bab541d3cc7741427406afcfb8dd21e449721
yuhi yamane <bird9.yuhi@gmail.com>
2025-02-14 01:36:40 +0900

    :sparkles: feat: パフォーマンス監視機能を追加
    PerformanceMonitorを使用し、FPS情報を表示する機能を実装。HeatmapViewerに関連コンポーネントを統合し、UIを調整。

--- src/pages/heatmap/tasks/[task_id]/HeatmapViewer.tsx
+++ src/pages/heatmap/tasks/[task_id]/HeatmapViewer.tsx
@@ -1,13 +1,16 @@
 import styled from '@emotion/styled';
+import { PerformanceMonitor } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { useCallback, useMemo, useRef, useState } from 'react';
 
 import type { HeatmapTask } from '@/modeles/heatmaptask.ts';
-import type { FC } from 'react';
+import type { PerformanceMonitorApi } from '@react-three/drei';
+import type { FC, ChangeEvent } from 'react';
 
 import { FlexColumn, FlexRow } from '@/component/atoms/Flex.tsx';
 import { HeatMapCanvas } from '@/pages/heatmap/tasks/[task_id]/HeatmapCanvas.tsx';
 import { HeatmapMenu } from '@/pages/heatmap/tasks/[task_id]/HeatmapMenu.tsx';
+import { PerformanceList } from '@/pages/heatmap/tasks/[task_id]/PerformanceList.tsx';
 import { dimensions } from '@/styles/style.ts';
 
 export type HeatmapViewerProps = {
@@ -18,12 +21,14 @@ export type HeatmapViewerProps = {
 const Component: FC<HeatmapViewerProps> = ({ className, task }) => {
   const [modelPath, setModelPath] = useState<string | null>(null);
   const [modelType, setModelType] = useState<'gltf' | 'glb' | 'obj' | null>(null);
+  const [dpr, setDpr] = useState(2);
+  const [performance, setPerformance] = useState<PerformanceMonitorApi>();
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const [isMenuOpen, setIsMenuOpen] = useState(true);
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (!files || files.length === 0) return;
 
@@ -63,15 +68,22 @@ const Component: FC<HeatmapViewerProps> = ({ className, task }) => {
     setIsMenuOpen(value);
   }, []);
 
+  const handleOnPerformance = useCallback((api: PerformanceMonitorApi) => {
+    setDpr(Math.floor(0.5 + 1.5 * api.factor));
+    setPerformance(api);
+  }, []);
+
   return (
     <FlexColumn className={className} align={'center'}>
       <input className={`${className}__inputfile`} type='file' accept='.gltf,.glb,.obj,.bin,.png' multiple onChange={handleFileChange} />
       <FlexRow className={`${className}__canvasBox`} wrap={'nowrap'}>
         <HeatmapMenu isMenuOpen={isMenuOpen} toggleMenu={handleMenuClose} />
         <div className={`${className}__canvas`}>
-          <Canvas camera={{ position: [500, 500, 500], fov: 50, near: 10, far: 10000 }} ref={canvasRef}>
+          <Canvas camera={{ position: [500, 500, 500], fov: 50, near: 10, far: 10000 }} ref={canvasRef} dpr={dpr}>
+            <PerformanceMonitor factor={1} onChange={handleOnPerformance} />
             <HeatMapCanvas modelPath={modelPath} modelType={modelType} pointList={pointList} />
           </Canvas>
+          {performance && <PerformanceList api={performance} className={`${className}__performance`} />}
         </div>
       </FlexRow>
     </FlexColumn>
@@ -92,7 +104,14 @@ export const HeatMapViewer = styled(Component)`
   }
 
   &__canvas {
+    position: relative;
     width: 100%;
     height: calc(90vh - ${dimensions.headerHeight}px);
+  }
+
+  &__performance {
+    position: absolute;
+    top: 0;
+    right: 10px;
   }
 `;
