801d0574687c0d20a0ae0f0822a781a25cda2526
yuhi yamane <bird9.yuhi@gmail.com>
2025-04-14 00:48:25 +0900

    :whale: feat: Add Docker setup for production
    Added Dockerfile, docker-compose, and Makefile for building and running the production container. Updated Next.js config for standalone output.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     pageExtensions: ['page.tsx'],
-    basePath: '/ludiscan/view'
+    basePath: '/ludiscan/view',
+    output: 'standalone',
 };
 
 export default nextConfig;
