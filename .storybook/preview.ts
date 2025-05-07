429e97ce7b1b4d11e2206f8a5b3bc6be43afa8f0
yuhi yamane <bird9.yuhi@gmail.com>
2025-05-07 12:29:30 +0900

    :hammer: chore: Storybook導入とGitHub Pages用デプロイ設定追加
    Storybook構成ファイルを設定し、デプロイ用のGitHub Actionsワークフローを追加。依存関係も更新済み。

---
+++ .storybook/preview.ts
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/react';
+
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        labels: { light: 'lightTheme', dark: 'darkTheme' },
+      },
+      options: ['light', 'dark'],
+    },
+  },
+  args: {
+    type: 'light',
+  }
+};
+export default preview;
