9c4cf32f4c3415277675376b0e6dfea8ee2d6ebe
yuhi yamane <bird9.yuhi@gmail.com>
2025-05-15 10:31:44 +0900

    fix

--- src/utils/heatmap/OfflineHeatmapProvider.tsx
+++ src/utils/heatmap/OfflineHeatmapProvider.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { Provider } from 'react-redux';
 
-import type { Env } from '@src/modeles/env';
 import type { FC } from 'react';
 
 import { ToastProvider } from '@src/component/templates/ToastContext';
@@ -11,12 +10,6 @@ import { HeatMapViewer } from '@src/pages/heatmap/tasks/[task_id]/HeatmapViewer'
 import { store } from '@src/store';
 import lightTheme from '@src/styles/light';
 import { useOfflineHeatmapDataService } from '@src/utils/heatmap/useOfflineHeatmapDataService';
-
-// オフラインモード用のEnv設定
-const offlineEnv: Env = {
-  HOSTNAME: 'https://offline.local',
-  API_BASE_URL: 'https://offline.local',
-};
 
 // Providerプロパティ
 interface OfflineHeatmapProviderProps {
@@ -36,7 +29,7 @@ export const OfflineHeatmapProvider: FC<OfflineHeatmapProviderProps> = ({ dataPa
       <ToastProvider position={'top-right'}>
         <QueryClientProvider client={queryClient}>
           <SharedThemeProvider initialTheme={lightTheme}>
-            <HeatMapViewer env={offlineEnv} dataService={dataService} />
+            <HeatMapViewer dataService={dataService} />
           </SharedThemeProvider>
         </QueryClientProvider>
       </ToastProvider>
