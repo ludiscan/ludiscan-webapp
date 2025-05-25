6c57a8dcd52e83ea9f3a382ff978aa39a96c2578
yuhi yamane <bird9.yuhi@gmail.com>
2025-05-26 01:10:47 +0900

    :children_crossing: feat: レスポンシブなレイアウトコンポーネント追加
    デスクトップとモバイルの両方で適応するレイアウトを切り替えるコンポーネントを作成。`DesktopLayout`と`MobileLayout`で条件に応じたレンダリングを実現。

---
+++ src/component/molecules/layouts.tsx
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import type { FC, ReactNode } from 'react';
+
+import { dimensions } from '@src/styles/style';
+
+const Component: FC<{ desktop: ReactNode; mobile: ReactNode }> = ({ desktop, mobile }) => {
+  const [isDesktopLayout, setIsDesktopLayout] = useState(true);
+
+  const matchMedia = useCallback(() => {
+    if (window.matchMedia(`(max-width: ${dimensions.mobileWidth}px)`).matches) {
+      setIsDesktopLayout(false);
+    } else {
+      setIsDesktopLayout(true);
+    }
+  }, []);
+  useEffect(() => {
+    document.addEventListener('DOMContentLoaded', matchMedia);
+    window.addEventListener('resize', matchMedia);
+    matchMedia();
+    return () => {
+      document.removeEventListener('DOMContentLoaded', matchMedia);
+      window.removeEventListener('resize', matchMedia);
+    };
+  }, [matchMedia]);
+  return <>{isDesktopLayout ? desktop : mobile}</>;
+};
+
+export const DesktopLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  return <Component desktop={children} mobile={null} />;
+};
+
+export const MobileLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  return <Component desktop={null} mobile={children} />;
+};
