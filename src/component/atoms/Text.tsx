caeddd721cf88b7c09d861e5d990b76b0918d773
yuhi yamane <bird9.yuhi@gmail.com>
2025-05-28 22:25:04 +0900

    :necktie: feat: Textコンポーネントにリンク機能を追加
    `href`プロパティを持つ場合にリンクとして動作するよう機能拡張。デフォルトで`_blank`をターゲットにし、セキュリティ属性を付与。

--- src/component/atoms/Text.tsx
+++ src/component/atoms/Text.tsx
@@ -12,7 +12,21 @@ export type TextProps = {
   style?: CSSProperties;
 };
 
-const Component: FC<TextProps> = ({ className, text, style }) => {
+export type TextLinkProps = TextProps & {
+  href: string;
+  target?: '_blank' | '_self';
+};
+
+const Component: FC<TextProps | TextLinkProps> = (props) => {
+  const { className, text, style } = props;
+  if ('href' in props) {
+    const { href, target = '_blank' } = props as TextLinkProps;
+    return (
+      <a className={className} href={href} style={{ ...style, color: 'unset' }} rel='noopener noreferrer' target={target}>
+        {text}
+      </a>
+    );
+  }
   return (
     <span className={className} style={style}>
       {text}
@@ -24,5 +38,6 @@ export const Text = styled(Component)`
   ${({ fontSize }) => fontSize && `font-size: ${fontSize}`};
   ${({ color }) => color && `color: ${color}`};
   font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
+  text-decoration: none;
   text-shadow: ${({ shadow }) => (shadow ? '0 0 4px rgba(0, 0, 0, 0.2)' : 'none')};
 `;
