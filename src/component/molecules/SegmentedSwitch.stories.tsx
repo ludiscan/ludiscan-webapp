0a936fa4a17c63a1f44a0720bfda18db20d39f2d
yuhi yamane <bird9.yuhi@gmail.com>
2025-06-04 12:13:27 +0900

    :dizzy: feat: SegmentedSwitchコンポーネントとStoryを追加
    カスタマイズ可能な2択スイッチコンポーネントを作成し、Storybookにサンプルを追加。複数フォントサイズやアクティブインジケーターのアニメーションに対応。

---
+++ src/component/molecules/SegmentedSwitch.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { FlexColumn } from '@src/component/atoms/Flex';
+import { SegmentedSwitch } from '@src/component/molecules/SegmentedSwitch';
+
+export default {
+  component: SegmentedSwitch,
+  controls: { hideNoControlsWarning: true },
+} as Meta;
+type Story = StoryObj<typeof SegmentedSwitch>;
+
+const Template: Story = {
+  render: (args) => {
+    return (
+      <FlexColumn gap={16}>
+        <SegmentedSwitch {...args} fontSize={'smallest'} />
+        <SegmentedSwitch {...args} fontSize={'small'} />
+        <SegmentedSwitch {...args} fontSize={'medium'} />
+        <SegmentedSwitch {...args} fontSize={'large1'} />
+        <SegmentedSwitch {...args} fontSize={'large2'} />
+        <SegmentedSwitch {...args} fontSize={'large3'} />
+        <SegmentedSwitch {...args} fontSize={'largest'} />
+      </FlexColumn>
+    );
+  },
+};
+
+export const Small: Story = {
+  ...Template,
+  args: {
+    options: ['Option 1', 'Option 2'],
+    value: 'Option 1',
+    onChange: () => {},
+    disabled: false,
+  },
+};
