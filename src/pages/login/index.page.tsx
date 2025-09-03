419cf04bb6bd5ff548e080f9d3536ba6a5b7f6e9
yuhi yamane <bird9.yuhi@gmail.com>
2025-09-03 14:02:53 +0900

    :dizzy: feat: update login page ui
    Replaced inline SVG with an external `google.svg` file for the Google login button. Improved styling and adjusted spacing for better layout consistency.

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,14 +1,15 @@
 import styled from '@emotion/styled';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useCallback, useMemo, useState } from 'react';
 
 import type { FC } from 'react';
 
 import { Button } from '@src/component/atoms/Button';
-import { Divider } from '@src/component/atoms/Divider';
 import { InlineFlexColumn } from '@src/component/atoms/Flex';
 import { VerticalSpacer } from '@src/component/atoms/Spacer';
 import { Text } from '@src/component/atoms/Text';
+import { LinedText } from '@src/component/molecules/LinedText';
 import { Modal } from '@src/component/molecules/Modal';
 import { OutlinedTextField } from '@src/component/molecules/OutlinedTextField';
 import { useToast } from '@src/component/templates/ToastContext';
@@ -56,33 +57,12 @@ const Content: FC<LoginPageProps> = ({ className }) => {
   const loginDisabled = useMemo(() => email === '' || password === '' || isLoading, [email, password, isLoading]);
   return (
     <Modal className={className} isOpen={true} onClose={onClose} title={'SignIn or SignUp'} closeOutside={false} style={{ minWidth: '300px' }}>
-      <InlineFlexColumn gap={24} className={`${className}__content`} align={'center'}>
+      <InlineFlexColumn gap={20} className={`${className}__content`} align={'center'}>
         <a className={`${className}__button google_login`} href={`${env.API_BASE_URL}/api/v0/auth/google`} target={'_self'}>
-          <svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48' className='LgbsSe-Bz112c'>
-            <g>
-              <path
-                fill='#EA4335'
-                d='M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z'
-              />
-              <path
-                fill='#4285F4'
-                d='M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z'
-              />
-              <path
-                fill='#FBBC05'
-                d='M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z'
-              />
-              <path
-                fill='#34A853'
-                d='M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z'
-              />
-              <path fill='none' d='M0 0h48v48H0z' />
-            </g>
-          </svg>
+          <Image src={'/google.svg'} alt={'google'} width={20} height={20} />
           <Text text={'Google で サインアップ'} color={theme.colors.text} fontSize={fontSizes.medium} />
         </a>
-        <Divider orientation={'horizontal'} />
-        <VerticalSpacer size={12} />
+        <LinedText text={'または'} lineThickness={'1px'} fullWidth={true} />
         <OutlinedTextField
           className={`${className}__email`}
           onChange={handleInputEmail}
@@ -103,7 +83,7 @@ const Content: FC<LoginPageProps> = ({ className }) => {
           fontSize={fontSizes.medium}
           maxLength={20}
         />
-        <VerticalSpacer size={12} />
+        <VerticalSpacer size={2} />
         <Button onClick={handleLogin} scheme={'primary'} fontSize={'medium'} width={'full'} disabled={loginDisabled}>
           <Text text={'Sign in'} />
         </Button>
