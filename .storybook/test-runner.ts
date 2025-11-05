c484c870e89c41794c4c5c8a2e3365ca36bfc8ca
yuhi yamane <bird9.yuhi@gmail.com>
2025-11-05 20:09:50 +0900

    fix

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -4,7 +4,7 @@ import { argosScreenshot } from '@argos-ci/storybook/test-runner';
 const config: TestRunnerConfig = {
   async postVisit(page, context) {
     // Take a screenshot of each story using Argos
-    await argosScreenshot(page, context.id, {
+    await argosScreenshot(page, context, {
       // Optional: Add custom options
       // fullPage: true,
       // element: '.my-custom-selector',
